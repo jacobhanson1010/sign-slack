58201f12c2fdcced4585d58aea49033123326d24
Unknown <jacob.hanson1010@gmail.com>
2019-02-15 21:27:11 -0600

    port finally

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def slash_command():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 33507))
-    app.run(host='0.0.0.0', port=33507, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
